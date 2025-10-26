09b3859ca3e0070cd2baa874702d7ce1c289ecfd
M.A.T.H <amruthavarshiniastakala@gmail.com>
2025-10-26 16:00:59 +0530

    Integrating nav bar

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './ContactUs.jsx'
-
+// Import the shared Navbar
+import Navbar from '../shared/components/NavBar'
+import '../shared/components/NavBar.css'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <Navbar />
     <App />
   </StrictMode>,
 )
